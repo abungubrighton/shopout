ff0908f7f7a41db31a49afa6c5888d66256b0f0b
abungubrighton <brighton@ibuqacapital.com>
2023-12-28 12:34:37 +0300

    chore:django backend project setup

---
+++ backend/base/products.py
@@ -0,0 +1,54 @@
+products = [{
+    "_id": "1",
+    "name": "AirpodS Wireless Bluetooth Headphones",
+    "image": "/images/airpods.jpg",
+    "description": "The best Bluetooth Headphones",
+    "brand": "Apple",
+    "category": "Electronics",
+    "price": 89.99,
+    "countInStock": 0,
+    "rating": 4.5,
+    "numReviews": 12
+
+
+}, {
+    "_id": "2",
+    "name": "Apple 11 pro 256 GB Memory",
+    "image": "/images/phone.jpg",
+    "description": "The best Iphone ever",
+    "brand": "Apple",
+    "category": "Electronics",
+    "price": 929.99,
+    "countInStock": 100,
+    "rating": 4.5,
+    "numReviews": 12
+
+
+}, {
+    "_id": "3",
+    "name": "Sonny Playstation 4 Pro White Version",
+    "image": "/images/phone.jpg",
+    "description": "The best Iphone ever",
+    "brand": "Apple",
+    "category": "Electronics",
+    "price": 399.99,
+    "countInStock": 10,
+    "rating": 5,
+    "numReviews": 12
+
+
+}, {
+    "_id": "4",
+    "name": "Amazon Echo Dot 3rd Generation",
+    "image": "/images/alexa.jpg",
+    "description": "Meet Echo Dot 3rd Generation the most popular smart speaker",
+    "brand": "Apple",
+    "category": "Electronics",
+    "price": 399.99,
+    "countInStock": 10,
+    "rating": 5,
+    "numReviews": 12
+
+
+}, ]
+
