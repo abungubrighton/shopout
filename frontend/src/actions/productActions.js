4b477bf243f1b98a424e514c706d8bc6624b1074
abungubrighton <brighton@ibuqacapital.com>
2024-01-01 12:07:03 +0300

    chore:bring redux to homescreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from '../constants/productConstants';
-const listProducts = () => async(dispatch) => {
+export const listProducts = () => async(dispatch) => {
     /**
      * Function that communicates to the reducer that handles list of products on the intentions (actions) to affect the data in store
      * dispatch  -> used to declare the action types that will affect the  products list
