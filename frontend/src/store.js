73b9d151ab36387b80085dad2a82dcb94dbc0221
abungubrighton <brighton@ibuqacapital.com>
2024-01-06 10:27:37 +0300

    chore:setup cart state

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { productDetailsReducer, productListReducer } from './reducers/productReducers'
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers'
+
+// load preloaded state from local storage
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [];
+
 const store = configureStore({
     reducer: {
         productList: productListReducer,
         productDetail: productDetailsReducer,
+        cart: cartReducer,
     },
+    preloadedState: {
+        cart: { cartItems: cartItemsFromStorage },
+    }
 });
-
 
 export default store;
