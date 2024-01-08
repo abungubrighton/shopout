c9a632c17e705e65327f2fc762797f5109f57a57
abungubrighton <brighton@ibuqacapital.com>
2024-01-08 21:52:45 +0300

    chore:implement add to cart functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,11 +1,11 @@
+import axios from "axios"
 import { CART_ADD_ITEM } from "../constants/cartConstants"
-
 export const addToCart = (id, qty) => async(dispatch, getState) => {
     const { data } = await axios.get(`/api/products/${id}`)
 
     dispatch({
         type: CART_ADD_ITEM,
-        payload: data
+        payload: {...data, qty }
     })
 
     // save the item to local storage , so we can load initial state of cart in the store from local storage
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import {useEffect} from 'react';
+import {Link, useParams,useSearchParams} from 'react-router-dom';
+import { Form,Row,Col,ListGroup,Image,Button,Card } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import { addToCart } from '../actions/cartActions';
 
 const CartScreen = () => {
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    const productId = id;
+    const [searchParams] = useSearchParams();
+    const qty = searchParams.get('qty');
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatch, productId, qty]);
+    const cart = useSelector(state => state.cart);
+    const { cartItems } = cart;
     return (
         <div>CartScreen</div>
+
     )
 }
 
