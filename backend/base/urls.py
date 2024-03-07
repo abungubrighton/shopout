03d575ce79947131a04ca24805e29f222ddff082
abungubrighton <brighton@ibuqacapital.com>
2024-03-07 19:27:25 +0300

    chore:Update User serializer

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('users/login/',views.MyTokenObtainPairView.as_view(), name="user_login"),
     path('products/',views.getProducts, name="products"),
     path('products/<str:pk>/',views.getProduct, name="product"),
+    path('users/profile/',views.getUserProfile, name="user-profile"),
 ]
