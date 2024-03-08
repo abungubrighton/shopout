2f4cb547ff121bcb5c19b25258b1fadc83b65208
abungubrighton <brighton@ibuqacapital.com>
2024-03-08 07:59:48 +0300

    chore:Cleanup Views and urls

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import get_object_or_404
-from .serializers import MyTokenObtainPairSerializer, ProductSerializer, UserSerializer, UserSerializerWithToken
-from .models import Product
+from base.serializers import MyTokenObtainPairSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
@@ -9,33 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 User = get_user_model()
-# Create your views here.
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes =[
-        '/api/products/',
-        'api/products/create/',
-        'api/products/upload/',
-        'api/products/<id>/reviews/',
-        'api/products/top/',
-        'api/products/<id>/',
-        'api/products/delete/<id>/',
-        'api/products/update/<id>/',
-        
-    ]
-    return Response(routes)
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products,many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getProduct(request,pk):
-    product = get_object_or_404(Product,pk=pk)
-    serializer = ProductSerializer(product,many=False)
-    return Response(serializer.data)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
